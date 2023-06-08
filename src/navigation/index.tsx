221b0293d8876b1e26372825afd0ed32711c0842
alireza.karami <alirezakaram.k74@gmail.com>
2023-06-08 14:54:44 +0330

    fix configs

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,7 +2,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {navigationRef} from './methods';
-import {SplashScreen} from '~/screens';
+import { SplashScreen } from '~/screens';
 
 export const mainStack = [
   {
